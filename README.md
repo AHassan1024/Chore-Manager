c432d493f4e972b55f48e55983eebafbe9c76e95
Ameena Hassan <ameenahassan1024@gmail.com>
2023-07-17 22:47:35 +0100

    Set up menu with maintain, add and remove

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Chore-Manager
+# Chore Manager
+
+Back-End in Python
+
+Use:
+1. Fork this repo
+2. cd into app/
+3. py chore-manager-app.py
+4. Use according to the menu
+
+
+What can it do?
+- [x] Maintain a list of chores
+- [x] Ask for chores
+- [x] Remove chores by index number
+- [ ] Chores not saved beyond shell
+- [ ] Menu options not numbered correctly
---
+++ app/chore-manager-app.py
@@ -0,0 +1,66 @@
+# Started: 16th July 2023
+# Last Edited: 17th July 2023
+# Next steps:
+# - Extend to support task searching
+# - Extend to support repeating tasks
+# - Refactor to move chore_list into a better form that supports sub lists
+# - Write to file
+# - Save list beyond the terminal command line and add sub lists
+
+chore_list = []
+
+
+def add_chore(chore):
+    # Add chore to list
+    chore_list.append(chore)
+    print(f"Chore: {chore} added!")
+
+
+def remove_chore(i):
+  # TODO: Introduce error handling on non-existent tasks.
+    chore_list.pop(i - 1)
+    print("Chore removed!\n Updated list:")
+    list_chores()
+
+
+def list_chores():
+    if len(chore_list) == 0:
+        print("No chores to display.")
+    else:
+        print("Chores:")
+        for index, chore in enumerate(chore_list, start=1):
+            print(f"{index}. {chore}")
+
+
+def print_menu():
+    print("\n--- Chore Manager ---")
+    print("1. List Chores")
+    print("2. Add Chore")
+    print("3. Remove Chore")
+    print("6. To exit, or 'q'")
+
+
+def main():
+    choice = ""
+    while choice != "q":
+        print_menu()
+        choice = input("Please select from the menu.\n")
+        if choice == "1":
+            list_chores()
+        elif choice == "2":
+            chore = input("Enter chore to add: ")
+            add_chore(chore)
+        elif choice == "3":
+            print("Choose a chore from the list to remove:")
+            list_chores()
+            remove_chore(int(input()))
+
+        elif (choice == "6") or (choice == "q"):
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == '__main__':
+    main()
